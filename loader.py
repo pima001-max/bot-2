261297adede3899a4cbe5b02da8fbb57f603fee8
pima001-max <maks.wm001@gmail.com>
2025-07-23 10:27:32 +0300

    commit002

--- loader.py
+++ loader.py
@@ -7,4 +7,4 @@ from data import config
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
-db = DatabaseManager('data/database.db')
+db = DatabaseManager('data/database.db')m
